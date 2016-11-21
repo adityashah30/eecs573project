46cdfd3d14cc1133219d8e1f863a4767defc1df8
Aditya Shah <adityashah30@gmail.com>
2016-11-21 14:14:12 -0500

    Summary tokenized

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,22 @@
+import sys
+import pandas as pd
+import gensim
+import nltk
+
+def load_data(fname):
+    data = pd.read_csv(fname)
+    return data
+
+def model_summary(data):
+    summary = data['Summary']
+    sentences = [nltk.word_tokenize(sentence) for sentence in summary]
+
 def main():
-    print "573 Term project"
+    if len(sys.argv) != 2:
+        print "Usage: python main.py <data.csv>"
+        sys.exit(1)
+    data = load_data(sys.argv[1])
+    model_summary(data)
 
 if __name__ == "__main__":
     main()
